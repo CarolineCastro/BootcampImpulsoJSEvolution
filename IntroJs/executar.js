da6167f604e5b953296c1cf82c979d40656b2b55
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-11 15:37:56 -0300

    Executando arquivos js com o node

---
+++ IntroJs/executar.js
@@ -0,0 +1,16 @@
+function returnEevenValues(array){
+    let evenNums = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i] % 2 === 0){
+            evenNums.push(array[i]);
+        } else {
+            console.log(`${array[i]} não é par!`);
+        }
+    }
+
+    console.log('Os núrmeros pares são: ',evenNums);
+}
+
+let array = [1, 2, 3, 4 , 5, 6, 7, 8];
+
+returnEevenValues(array);
