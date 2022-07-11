3863123a7b7c96941f1a08423ca921db83ddd3d6
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-11 14:53:05 -0300

    Funções em js

--- IntroJs/intro.js
+++ IntroJs/intro.js
@@ -1,3 +1,5 @@
+//DECLARAÇÃO DE VARIÁVEIS
+
 var preco = 2; //boas práticas pedem para nomear as váriaveis de forma clara.
 var desconto = 0.2;
 const IMPOSTO = 2; //constantes devem ser nomeadas em caixa alta.
@@ -7,3 +9,12 @@ preco = preco - desconto;
 var total = preco + IMPOSTO;
 
 //em js as váriavies nn precisam ser pré-declaradas com um tipo, a atribuição já define o tipo da variável.
+
+//DECLARAÇÃO DE FUNÇÕES
+
+function soma (a, b){
+    console.log(a + b); //apenas para ver se a função está retornando o esperado.
+    return a + b;
+}
+
+soma (3, 5);
