45243964665c2a11b123568c486581380cc5dd56
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-11 14:45:30 -0300

    Entendendo variáveis em js

---
+++ IntroJs/intro.js
@@ -0,0 +1,9 @@
+var preco = 2; //boas práticas pedem para nomear as váriaveis de forma clara.
+var desconto = 0.2;
+const IMPOSTO = 2; //constantes devem ser nomeadas em caixa alta.
+
+preco = preco - desconto;
+
+var total = preco + IMPOSTO;
+
+//em js as váriavies nn precisam ser pré-declaradas com um tipo, a atribuição já define o tipo da variável.
