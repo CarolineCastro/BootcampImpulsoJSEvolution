32fe860c791d3127328505a87120e2ac2305dce7
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-14 15:21:29 -0300

    Sintaxe JS: Funções e suas propriedades

---
+++ SintaxeJS/4-funções/script.js
@@ -0,0 +1,17 @@
+/*FUNÇÕES*/
+
+//SEM PARÂMETROS
+function funcao() {
+    //do stuff
+}
+
+funcao();
+
+//COM PARÂMENTROS
+function funcao(primeiro, segundo /*parâmetros*/){
+    //do stuff
+}
+
+funcao();
+
+
