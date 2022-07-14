2f9a49156aaed64850f056707e7238a6181fa3c7
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-14 15:32:31 -0300

    Sintaxe em JS: Aprofundamento em funcoes

--- SintaxeJS/4-funções/script.js
+++ SintaxeJS/4-funções/script.js
@@ -14,4 +14,37 @@ function funcao(primeiro, segundo /*parâmetros*/){
 
 funcao();
 
+/*APROFUNDAMENTO EM FUNÇÕES*/
+
+    /*TIPOS DE FUNÇÃO*/
+
+//DECLARATIVAS: precisão de um nome
+function funcao(){
+    //do stuff
+}
+
+funcao();
+
+//EXPRESSÕES DE FUNÇÃO
+    //COM NOME
+    var funcao = function nome(){
+        //do stuff
+    }
+
+    funcao();
+
+    //SEM NOME
+    var funcao = function(){
+        //do stuff
+    }
+
+    funcao();
+
+//ARROW FUNCTION: funções de sintaxe curta
+var funcao = () => {
+    //do stuff
+}
+
+funcao();
+
 
