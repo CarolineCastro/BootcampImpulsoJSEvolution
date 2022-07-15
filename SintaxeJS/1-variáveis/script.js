edfcd16159dbc6fd7314159ada607b2168414483
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-15 13:21:51 -0300

    Sintaxe em JS: aprofundando conhecimentos em variáveis e tipos primitivos

--- SintaxeJS/1-variáveis/script.js
+++ SintaxeJS/1-variáveis/script.js
@@ -80,3 +80,91 @@ var or = true || false;
 
 //NOT
 var not = !true;
+
+
+/*HOSTING*/
+//váriaveis não declaradas são automaticamnte lidas como var. Isso não acontece caso, posteriormente a váriavel seja declarada como let
+
+numberOne = 1;
+console.log(numberOne + 2);
+
+
+//O let como é escopo de bloco, nn altera valores de uma var, msm sendo declarada depois. E variáveis declaradas com let nn podem ser redeclaradas. Ou seja, não fazem hosting.
+
+var firstName = "João";
+var lastName = "Souza";
+
+if(firstName === "João") {
+    var firstName = "Pedro";
+    let lastName = "Silva";
+}
+
+console.log(firstName, lastName); // a saída será Pedro e Souza
+
+//constantes não fazem hosting.
+
+/*APROFUNDANDO EM STRINGS*/
+
+//CONCATENAÇÃO DE STRINGS
+let nome = "João";
+let sobrenome = "Pedro";
+
+let concatenado = nome.concat(sobrenome);
+    //ou
+concatenado = nome + sobrenome;
+concatenado = nome + " " + sobrenome;
+concatenado = nome + "\n" + sobrenome;
+concatenado = `${nome} ${sobrenome}`;
+concatenado = `${nome} 
+${sobrenome}`;
+
+//TAMANHO DE STRINGS
+let tamanho = nome.length;
+
+//STRINGS COMO OBJETOS
+let exemplo = new String("blablabla");
+
+    //esse obj retorna o conteúdo indexado e todos os método da String. Por default, o js entende q o tipo primitivo string é a mesma coisa que o obj, por isso é possível usar os métodos do obj String no tipo primitivo. A principal diferença é q no obj a string é mais complexa.
+
+//SPLIT
+let frase = "Olá, tudo bem?"
+frase.split(""); //irá retornar cada caracter da frase separados, inclundo o espaaço e a vírgula.
+
+frase.split(" "); //irá retornar cada palavra, sem os espaços e a vírgula.
+
+//INCLUDES
+frase.includes("tudo"); //retorna um booleano
+
+//STARTS WITH
+frase.startsWith("O"); //tbm retorna um booleano
+
+//ENDS WITH
+frase.endsWith("?"); //tbm retorna um booleano
+
+//REPLACE
+let stringModificada = frase.replace(",", "!"); //irá retornar uma nova string.
+
+
+
+/*APROFUNDAMENTO EM NÚMEROS*/
+
+//MATH 
+//É um objeto com vários métodos q retornam valores ou equações mátemáticas.
+
+Math.PI; //retorna PI
+
+let fracao = 5/3;
+Math.floor(fracao); //retorna o valor arredondado para baixo
+Math.ceil(fracao); //retorna o valor arredondado para baixo
+
+//TO STRING
+let fracaoString = fracao.toString();
+
+    //o to string converte qualquer tipo primitivo para string, números, booleanos...
+
+
+
+
+
+
+
