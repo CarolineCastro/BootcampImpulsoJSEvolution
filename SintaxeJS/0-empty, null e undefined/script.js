ccb30c496796f5b2d02c2c5133eae8cc6d9c7431
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-15 13:45:48 -0300

    Sintaxe em JS: aprendendo sobre valores falsos, empty, null e undefined.

---
+++ SintaxeJS/0-empty, null e undefined/script.js
@@ -0,0 +1,13 @@
+/*EMPTY, NULL E UNDEFINED*/
+
+//EMPTY - é um valor vazio. Se é um número significa q foi inicializado com 0, string com aspas vazias e assim por diante;
+
+//NULL - é um valor inexistente. Vc declarou uma variável e a tornou NULL propositálmente.
+
+//UNDEFINED - nem chegou a ser declarado.
+
+console.log(abacaxi); //irá retorna undefined
+
+let abacaxi = null; //agora é uma várivel do tipo objeto q retorna um valor falso, mas nn o falso booleano.
+
+let abacaxi1; //é um valor vazio
