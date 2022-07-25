5b196519cff81f966d0b9ce6a72c98163a7f9cc2
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-25 18:24:39 -0300

    Typescript: interfaces e tipos

--- Typescript/intro/index.ts
+++ Typescript/intro/index.ts
@@ -1,4 +1,28 @@
-function soma(a: number, b: number){
-    return a + b;
+//types
+
+//interfaces
+interface IAnimal {
+    nome: string;
+    tipo: 'terrestre' | 'aquático';
+
+    executarRugido(alturaEmDecibeis: number): void;
 }
 
+interface IFelino extends IAnimal {
+    visaoNoturna: boolean;
+}
+
+const animal: IAnimal = {
+    nome: 'Elefante',
+    tipo: 'terrestre',
+    executarRugido: (alturaEmDecibeis) => (`${alturaEmDecibeis}db`)
+
+}
+
+animal.executarRugido('s'); //nn vai aceitar string, pois espera um tipo number
+
+const felino: IFelino = {
+    nome: 'Leão',
+    tipo: "terrestre",
+    visaoNoturna: true,
+}
