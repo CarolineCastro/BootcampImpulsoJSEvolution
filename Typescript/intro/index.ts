b031b1e7874463651906cce272c895890fe6dfea
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-28 12:50:16 -0300

    Typescript: desenvolvendo condicionais a partir de parâmentros

--- Typescript/intro/index.ts
+++ Typescript/intro/index.ts
@@ -1,4 +1,4 @@
-/*interfaces: usamos para definir contratos de estruturas de dados, de classes;
+//INTERFACES: usamos para definir contratos de estruturas de dados, de classes;
 interface IAnimal {
     nome: string;
     tipo: 'terrestre' | 'aquático';
@@ -15,7 +15,7 @@ interface ICanino extends IAnimal {
     porte: 'pequeno' | 'medio' | 'grande';
 }
 
-//type: usamos para fazer junções das interfaces;
+//TYPE: usamos para fazer junções das interfaces;
 type IDomestico = IFelino | ICanino;
 
 const animal: IDomestico = {
@@ -23,17 +23,16 @@ const animal: IDomestico = {
     nome: 'cachorro',
     porte: 'medio',
     tipo: 'terrestre',
-}*/
+}
 
-/*Tratando a tag input
+//TRATANDO A TAG INPUT
 const input = document.getElementById('input') as HTMLInputElement;
 
 input.addEventListener('input', (event) => {
     const i = event.currentTarget as HTMLInputElement;
-});*/
+});
 
-//Generic types
-
+//TIPOS GENÉRICOS
 function addApendiceLista<T>(array: T[], valor: T) /*implica numa função genérica*/{
     return array.map(() => valor);
 }
@@ -41,3 +40,26 @@ function addApendiceLista<T>(array: T[], valor: T) /*implica numa função gené
 addApendiceLista([1, 2, 3], 1); //como nn foi definido o q a função recebe e retorna, então todos os valores são do tipo any
 
 //após o definir um tipo genérico T, a função lida com o tipo do dado enviado.
+
+
+//DESENVOLVENDO CONDICIONAIS A PARTIR DE PARÂMENTRSO
+
+interface IUsuario {
+    id: string;
+    email: string;
+}
+
+interface IAdmin extends IUsuario {
+    cargo: 'gerente' | 'coordenador' | 'supervisor';
+}
+
+function redirecione(usuario: IUsuario | IAdmin) { //usando a condição para aceitar dois tipos diferentes
+    if( 'cargo' in usuario){
+        //redirecionar para a área de administração
+    }
+
+    //se nn redirecionar para a área do usuário
+}
+
+
+
