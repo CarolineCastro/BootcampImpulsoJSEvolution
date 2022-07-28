6f6e3e6e560f5ffc18c4d07e252a3eb8ce863f27
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-07-28 12:37:15 -0300

    Typescript: tipos genéricos

--- Typescript/intro/index.ts
+++ Typescript/intro/index.ts
@@ -25,9 +25,19 @@ const animal: IDomestico = {
     tipo: 'terrestre',
 }*/
 
-
+/*Tratando a tag input
 const input = document.getElementById('input') as HTMLInputElement;
 
 input.addEventListener('input', (event) => {
     const i = event.currentTarget as HTMLInputElement;
-});
+});*/
+
+//Generic types
+
+function addApendiceLista<T>(array: T[], valor: T) /*implica numa função genérica*/{
+    return array.map(() => valor);
+}
+
+addApendiceLista([1, 2, 3], 1); //como nn foi definido o q a função recebe e retorna, então todos os valores são do tipo any
+
+//após o definir um tipo genérico T, a função lida com o tipo do dado enviado.
