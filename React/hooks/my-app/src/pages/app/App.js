53ec7063ec221c5126102c6839101c342d6f5720
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-03 19:23:26 -0300

    React: Intro a React Hooks... Iniciando um novo projeto, criando estrutura básica e setendo testes

---
+++ React/hooks/my-app/src/pages/app/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import narutoImg from '../../images/naruto.png'
+
+function App(){
+    return (
+        <div>
+            <p>quote xpto - speaker</p>
+            <button>Quote No Jutsu</button>
+            <img src={narutoImg} alt="Naruto with a kunai"/>
+        </div>
+    )
+}
+
+export default App;
+
+
