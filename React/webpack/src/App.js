204f7dd3acb85bb6493e5d7ca9eecd6f9135fe72
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-02 21:03:10 -0300

    React: Webpack, instalação e configuração

---
+++ React/webpack/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from "./Button.js";
+
+const App = () => {
+    return (
+        <div>
+            <p>Digital Inovation One</p>
+            <p>Bem vindo a nossa aula =D</p>
+            <div>
+                <Button></Button>
+            </div>
+        </div>
+    );
+};
+export default App;
