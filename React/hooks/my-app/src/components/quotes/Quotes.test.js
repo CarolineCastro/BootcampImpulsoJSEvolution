b910a9f158b3f34236c771626d52239a8e70f580
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-03 21:12:48 -0300

    React Hooks: adicionando teste para o botão quando for clicado

--- React/hooks/my-app/src/components/quotes/Quotes.test.js
+++ React/hooks/my-app/src/components/quotes/Quotes.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Quotes from './Quotes.js';
 
 const quote = 'test quote';
@@ -15,4 +15,16 @@ test('renders received quote, speaker and a button', () => {
     expect(speakerEl).toBeInTheDocument();
     expect(buttonEl).toBeInTheDocument();
     
+});
+
+test('calls a callback when button is pressed', () => {
+    const callback = jest.fn();
+
+    render(<Quotes quote={quote} speaker={speaker}  onUpdate={callback} />);
+
+    const buttonEl = screen.getByRole('button');
+
+    fireEvent.click(buttonEl);
+
+    expect(callback).toHaveBeenCalledTimes(1);
 });
