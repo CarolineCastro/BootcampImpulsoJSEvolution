5c8b8438b7e9d2e5e8fee2aa254bb092cb3baa9e
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-04 01:33:21 -0300

    React States & Effects: Criando um componente q imita o contador do ifood usando state com aplicações para css além das funcionalidades da aula anterior

--- React/states-effects/my-app/src/Components/App/App.js
+++ React/states-effects/my-app/src/Components/App/App.js
@@ -1,13 +1,14 @@
 
+import IfoodCounter from '../IfoodCounter/IfoodCounter';
 import './App.css';
 //import Counter from '../Counter/Counter';
-import SmartCounter from '../SmartCounter/SmartCounter';
+//import SmartCounter from '../SmartCounter/SmartCounter';
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
-      <SmartCounter />
+      <IfoodCounter/>
     </>
 
   );
