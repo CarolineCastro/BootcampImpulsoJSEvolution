8c5cf8e77493ebcb7fd4bcf3cec385017793a59c
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-05 15:48:09 -0300

    Node REST-BD: definindo o método create para inserção de usuários

--- Node/rest-2/src/routes/users.route.ts
+++ Node/rest-2/src/routes/users.route.ts
@@ -4,11 +4,14 @@ import userRepository from '../repositories/user.repository';
 
 const usersRoute = Router();
 
+//GET /USERS
 usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction) => {
     const users = await userRepository.findAllUsers();
     res.status(StatusCodes.OK).send(users);
 });
 
+
+//GET /USERS/:UUID
 usersRoute.get('/users/:uuid', async (req: Request <{  uuid: string  }>, res: Response, next: NextFunction) => {
     
     const uuid  = req.params.uuid;
@@ -16,12 +19,17 @@ usersRoute.get('/users/:uuid', async (req: Request <{  uuid: string  }>, res: Re
     res.status(StatusCodes.OK).send(user);
 });
 
-usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
-    const newUser = req.body;
 
-    res.status(StatusCodes.CREATED).send(newUser);
+//POST /USERS
+usersRoute.post('/users', async (req: Request, res: Response, next: NextFunction) => {
+    const newUser = req.body;
+    const uuid = await userRepository.create(newUser);
+
+    res.status(StatusCodes.CREATED).send(uuid);
 });
 
+
+//PUT /USERS/:UUID
 usersRoute.put('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
 
     const uuid = req.params.uuid;
@@ -32,6 +40,8 @@ usersRoute.put('/users/:uuid', (req: Request, res: Response, next: NextFunction)
     res.status(StatusCodes.OK).send({ uuid });
 });
 
+
+//DELETE /USERS/:UUID
 usersRoute.delete('/users/:uuid', (req: Request, res: Response, next: NextFunction) => {
     res.sendStatus(StatusCodes.OK);
 });
