8021cabe675ef5eb36bc5b4bdaf7de249dd76cfa
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-05 05:04:49 -0300

    REST: criação do users.route.ts, já implementando o get, post, put e delete

--- Node/rest/src/index.ts
+++ Node/rest/src/index.ts
@@ -1,13 +1,22 @@
 import express, { Request, Response, NextFunction } from "express";
+import usersRoute from "./routes/users.route";
 
 const app = express();
 
+//Condigurações da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//Configurações de Rotas
+app.use(usersRoute);
+
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
 
-    res.status(200).send({foo: 'bar'});
+    res.status(200).send({foo: 'Sucesso!'});
     
 });
 
+//Inicialização do servidor
 app.listen(3000, () => {
     console.log('Aplicaão executando na porta 3000!');
 });
