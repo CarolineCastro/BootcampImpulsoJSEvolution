4024632005d5f901d91c18a99e25823a7bcd1d91
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-05 16:43:12 -0300

    Node REST-BD: Desenvolvendo um Error Handling

--- Node/rest-2/src/repositories/user.repository.ts
+++ Node/rest-2/src/repositories/user.repository.ts
@@ -1,4 +1,4 @@
-
+import  DatabaseError  from "../models/errors/database.error.model";
 import db from "../db";
 import User from "../models/user.model";
 
@@ -17,18 +17,24 @@ class UserRepository {
         return rows || [];
     }
     
-    async findById (uuid: string): Promise<User> {  
-        const query = `
-            SELECT uuid, username
-            FROM application_user
-            WHERE uuid = $1
-        `;
+    async findById (uuid: string): Promise<User> {
+        try{
+            const query = `
+                SELECT uuid, username
+                FROM application_user
+                WHERE uuid = $1
+            `;
 
-        const values = [uuid];
-        const { rows } = await db.query<User>(query, values);
-        const [ user ] = rows;
+            const values = [uuid];
+            const { rows } = await db.query<User>(query, values);
+            const [ user ] = rows;
 
-        return user;
+            return user;
+
+        } catch(error){
+            throw new DatabaseError('Erro na consulta por ID', error);
+        }  
+       
 
     }
 
--- Node/rest-2/src/routes/users.route.ts
+++ Node/rest-2/src/routes/users.route.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import StatusCodes from 'http-status-codes';
+import DatabaseError from '../models/errors/database.error.model';
 import userRepository from '../repositories/user.repository';
 
 const usersRoute = Router();
@@ -13,10 +14,16 @@ usersRoute.get('/users', async (req: Request, res: Response, next: NextFunction)
 
 //GET /USERS/:UUID
 usersRoute.get('/users/:uuid', async (req: Request <{  uuid: string  }>, res: Response, next: NextFunction) => {
-    
-    const uuid  = req.params.uuid;
-    const user = await userRepository.findById(uuid);
-    res.status(StatusCodes.OK).send(user);
+
+    try {
+        const uuid  = req.params.uuid;
+        const user = await userRepository.findById(uuid);
+        res.status(StatusCodes.OK).send(user);
+        
+    } catch (error) {
+        next(error);
+    }
+      
 });
 
 
