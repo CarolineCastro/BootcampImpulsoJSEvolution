b1ecbba58020fac043f4d9293c8c5335092927d6
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-05 14:40:39 -0300

    Node REST-BD: conectando o arquivo db.ts com o bd -- criando pasta repositories com o arquivo user.repository.ts e pasta model com o arquivo user.model.ts -- alimentando os novos arquivos

--- Node/rest-2/src/db.ts
+++ Node/rest-2/src/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 
-const connectionString = '';
+const connectionString = 'postgres://btcdpdpq:zwjTyby1lTD2V4vFwbOToN-tCVdeqrBp@kesavan.db.elephantsql.com/btcdpdpq';
 const db = new Pool({ connectionString });
 
 export default db;
