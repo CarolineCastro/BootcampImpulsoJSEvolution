395cff8a0681a4bc22a1c9f998308afdcb116f0d
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-05 17:59:17 -0300

    Node Microserviço de Autenticação: adicionando rotas de autorização e tratamento de erros

---
+++ Node/ms-auth-tokens/readme.md
@@ -0,0 +1,4 @@
+Microsserviço de Autenticação com NodeJS
+
+O Banco de Dados está em: https://www.elephantsql.com/
+
---
+++ Node/ms-auth-tokens/src/db.ts
@@ -0,0 +1,8 @@
+import { Pool } from "pg";
+
+
+const connectionString = 'postgres://btcdpdpq:zwjTyby1lTD2V4vFwbOToN-tCVdeqrBp@kesavan.db.elephantsql.com/btcdpdpq';
+
+const db = new Pool({ connectionString });
+
+export default db;
