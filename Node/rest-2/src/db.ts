9c10add3c7115469b08340dbd66666d76d933381
Caroline Castro <carolynedecastrosilva@gmail.com>
2022-08-05 19:51:38 -0300

    NODE REST-BD: removendo link do bd por sergurança

--- Node/rest-2/src/db.ts
+++ Node/rest-2/src/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from "pg";
 
 
-const connectionString = 'postgres://btcdpdpq:zwjTyby1lTD2V4vFwbOToN-tCVdeqrBp@kesavan.db.elephantsql.com/btcdpdpq';
+const connectionString = '';
 
 const db = new Pool({ connectionString });
 
